34621414ed152e94beac8aa6f583c8d7f9cb5fdb
khdmolop <83680629+khdmolop@users.noreply.github.com>
2021-11-23 04:46:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp11overrloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person p1 = new person();
+            p1.print();
+            p1.print(10);
+            p1.print("khaled");
+            p1.print(10, "khaled");
+        }
+    }
+}
